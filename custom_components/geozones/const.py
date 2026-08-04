417a8f79ef053694d8abdc15962653f73adb6bbf
dualityps <4126225+ticstyle@users.noreply.github.com>
2026-08-04 14:34:59 +0200

    Add show_in_sidebar configuration option

--- custom_components/geozones/const.py
+++ custom_components/geozones/const.py
@@ -12,9 +12,11 @@ CONF_WIFI_SSID_SENSOR: Final = "wifi_ssid_sensor"
 CONF_HOME_SSIDS: Final = "home_ssids"
 CONF_HOME_ZONE: Final = "home_zone"
 CONF_USE_CUSTOM_ZONES: Final = "use_custom_zones"
+CONF_SHOW_IN_SIDEBAR: Final = "show_in_sidebar"
 
 DEFAULT_HOME_ZONE: Final = "zone.home"
 DEFAULT_USE_CUSTOM_ZONES: Final = True
+DEFAULT_SHOW_IN_SIDEBAR: Final = True
 CUSTOM_ZONES_FILENAME: Final = "geozones_custom_zones.geojson"
 
 ATTR_CONTAINING_ZONES: Final = "containing_zones"
